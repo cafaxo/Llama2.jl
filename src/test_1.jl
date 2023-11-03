f683a1d233c29ecc97b78fb520443dddf58ecfc8
zsz00 <zsz6174@gmail.com>
2023-11-03 20:02:53 +0800

    update

--- src/test_1.jl
+++ src/test_1.jl
@@ -13,6 +13,7 @@ file_name = "/home/zhangyong/codes/julia_learn/others/llm/models/llama2/llama-2-
 # https://huggingface.co/about0/llama-v2-chinese-ymcui-alpaca-GGML-13B/blob/main/llama-v2-chinese-alpaca-13B-Q4_K_S.ggml
 # file_name = "/home/zhangyong/codes/julia_learn/others/llm/models/llama2/llama-v2-chinese-alpaca-13B-Q4_K_S.ggml"  # ok, 可用. 8.3G内存
 
+# https://huggingface.co/TheBloke/SciPhi-Self-RAG-Mistral-7B-32k-GGUF/blob/main/sciphi-self-rag-mistral-7b-32k.Q4_K_S.gguf
 file_name = "/home/zhangyong/codes/julia_learn/others/llm/models/llama2/sciphi-self-rag-mistral-7b-32k.Q4_K_S.gguf"  # ok, 可用. 8.3G内存
 
 
@@ -21,7 +22,8 @@ model = load_gguf_model(file_name)
 # sample(model, "The Julia programming language is"; temperature = 1.0f0)
 # sample(model, "What are the prime numbers up to 100?")  # ; temperature = 0.8f0
 # sample(model, "The capital of China is"; temperature = 1.0f0)  # The capital of China is 中国的首都是
-sample(model, "China"; temperature = 1.0f0)
+sample(model, "China"; temperature = 1.0f0)  
 
 # julia -t auto --project=. src/test_1.jl
+# 有空格适配问题
 # 测试的中文聊天效果很差劲.没用.  2023.10.25
